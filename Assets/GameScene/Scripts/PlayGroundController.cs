810974ca35245b0824e6335a280346f7b5b5b272
YoshinakaHiroki <hiroki.yoshinaka777@gmail.com>
2019-08-28 00:34:34 +0900

    [add]PlayGroundController.cs
    背景を移動させることで見かけ上、Playerが上に進んでいるかのようになります。

---
+++ Assets/GameScene/Scripts/PlayGroundController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Hunkoro
+{
+
+    /// <summary>
+    /// 背景を動かすことでPlayerのy軸を移動させずに移動しているかのように見せかけます。
+    /// </summary>
+    public class PlayGroundController : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject player;
+        private PlayerController playerController;
+        //
+        private Rigidbody2D rigidbody;
+        // Start is called before the first frame update
+        void Start()
+        {
+            playerController = player.GetComponent<PlayerController>();
+            this.rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            switch (playerController.GetGameMode())
+            {
+                case GAMEMODE.STAY_START:
+                    StayStart();
+                    break;
+                case GAMEMODE.PLAY:
+                    Play();
+                    break;
+                case GAMEMODE.GAMEOVER:
+                    GameOver();
+                    break;
+                case GAMEMODE.CLEAR:
+                    Clear();
+                    break;
+            }
+            //GAMEMODE.STAY_STARTで呼び出される
+            void StayStart()
+            {
+
+            }
+
+            //GAMEMODE.PLAYで呼び出される
+            void Play()
+            {
+                rigidbody.velocity = new Vector2(0, playerController.GetSpeed());
+                if (transform.position.y < -15)
+                {
+                    transform.position = new Vector3(0, 25, 0);
+                }
+            }
+
+            //GAMEMODE.GAMEOVERで呼び出される
+            void GameOver()
+            {
+
+            }
+
+            //GAMEMODE.CLEARで呼び出される
+            void Clear()
+            {
+
+            }
+        }
+    }
+}
--- Assets/GameScene/Scripts/PlayerController.cs
+++ Assets/GameScene/Scripts/PlayerController.cs
@@ -1,19 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Hunkoro
 {
+    //Playerの状態=ゲーム全体の状態
+    public enum GAMEMODE
+    {
+        STAY_START,PLAY,GAMEOVER,CLEAR,
+    }
+    //Playerの大きさの段階
+    public enum PLAYER_LEVEL
+    {
+        FIRST,SECOND,THIRD,
+    }
     /// <summary>
-    /// "Player"の挙動を指定します。
+    /// "Player"の挙動を記述します。
     /// </summary>
     public class PlayerController : MonoBehaviour
     {
+        //状態
+        [SerializeField]
+        private GAMEMODE GameMode;
+        //アクセサメソッド
+        public GAMEMODE GetGameMode()
+        {
+            return GameMode;
+        }
+        //レベル
+        [SerializeField]
+        private PLAYER_LEVEL playerLevel;
+        //アクセサメソッド
+        public PLAYER_LEVEL GetPlayerLevel()
+        {
+            return playerLevel;
+        }
         //移動に使うので取得
         private new Rigidbody2D rigidbody;
-        //速度を指定
+        //全体の速度を指定?
         [SerializeField]
-        private float speed = 1.0f;
+        private float speed;
+        //アクセサメソッド
+        public float GetSpeed()
+        {
+            return speed;
+        }
 
 
         // Start is called before the first frame update
@@ -21,13 +53,66 @@ namespace Hunkoro
         {
             //Rigidbody2dコンポーネントを取得
             this.rigidbody = GetComponent<Rigidbody2D>();
+            //状態、大きさをセット
+            GameMode = GAMEMODE.PLAY;
+            playerLevel = PLAYER_LEVEL.FIRST;
         }
 
         // Update is called once per frame
         void Update()
         {
-            //x方向の移動は変更せずにy方向に等速度運動
-            rigidbody.velocity = new Vector2(rigidbody.velocity.x, speed);
+            switch(GameMode)
+            {
+                case GAMEMODE.STAY_START:
+                    StayStart();
+                    break;
+                case GAMEMODE.PLAY:
+                    Play();
+                    break;
+                case GAMEMODE.GAMEOVER:
+                    GameOver();
+                    break;
+                case GAMEMODE.CLEAR:
+                    Clear();
+                    break;
+            }
+        }
+
+        //GAMEMODE.STAY_STARTで呼び出される
+        void StayStart()
+        {
+
+        }
+
+        //GAMEMODE.PLAYで呼び出される
+        void Play()
+        {
+            switch(playerLevel)
+            {
+                case PLAYER_LEVEL.FIRST:
+                    break;
+                case PLAYER_LEVEL.SECOND:
+                    break;
+                case PLAYER_LEVEL.THIRD:
+                    break;
+            }
+        }
+
+        //GAMEMODE.GAMEOVERで呼び出される
+        void GameOver()
+        {
+
+        }
+
+        //GAMEMODE.CLEARで呼び出される
+        void Clear()
+        {
+
+        }
+
+        public static explicit operator PlayerController(GameObject v)
+        {
+            throw new NotImplementedException();
         }
     }
 }
