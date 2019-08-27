fcc640c78068c04af08e825ab15b9be1758d9aed
YoshinakaHiroki <hiroki.yoshinaka777@gmail.com>
2019-08-27 17:15:06 +0900

    [add]PlayerController.cs
    Playerの縦移動を実装しました。
    Implemented vertical movement of Player.

---
+++ Assets/GameScene/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hunkoro
+{
+    /// <summary>
+    /// "Player"の挙動を指定します。
+    /// </summary>
+    public class PlayerController : MonoBehaviour
+    {
+        //移動に使うので取得
+        private new Rigidbody2D rigidbody;
+        //速度を指定
+        [SerializeField]
+        private float speed = 1.0f;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //Rigidbody2dコンポーネントを取得
+            this.rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //x方向の移動は変更せずにy方向に等速度運動
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, speed);
+        }
+    }
+}
