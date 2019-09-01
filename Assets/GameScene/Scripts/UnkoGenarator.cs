12531ff3585bea2b9c8d567c279955d020da555c
YoshinakaHiroki <hiroki.yoshinaka777@gmail.com>
2019-09-02 02:35:08 +0900

    [add]UnkoGenarator.cs,UnkoPrefab.prefab

---
+++ Assets/GameScene/Scripts/UnkoGenarator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hunkoro
+{
+    public class UnkoGenarator : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject unkoPrefab = null;
+        [SerializeField]
+        private GameObject player = null;
+        private PlayerController playerController;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            playerController = player.GetComponent<PlayerController>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            switch(playerController.GetPlayerLevel())
+            {
+                case PLAYER_LEVEL.FIRST:
+                    break;
+                case PLAYER_LEVEL.SECOND:
+                    break;
+                case PLAYER_LEVEL.THIRD:
+                    break;
+                case PLAYER_LEVEL.FORTH:
+                    break;
+                case PLAYER_LEVEL.FIFTH:
+                    break;
+            }
+        }
+    }
+}
