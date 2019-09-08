929363567ccfcc9440d8134faad3a9d5478c5c2d
YoshinakaHiroki <hiroki.yoshinaka777@gmail.com>
2019-09-08 15:10:09 +0900

    [add]ClearScoreManager.cs,ClearTimeManager.cs
    クリア画面でのスコア表示を実装しました。
    Implemented the score display on the clear screen.

---
+++ Assets/ClearScene/Scripts/ClearTimeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+namespace Hunkoro
+{
+    public class ClearTimeManager : MonoBehaviour
+    {
+        private Text text = null;
+        // Start is called before the first frame update
+        void Start()
+        {
+            text = GetComponent<Text>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float time = PlayerController.GetTimeScore();
+            time = Mathf.Round(time * 10) / 10;
+            text.text = String.Format("{0:0.0}", time);
+        }
+    }
+}
