2aff879abace4adf836b89183b3edb188955d08a
YoshinakaHiroki <hiroki.yoshinaka777@gmail.com>
2019-09-09 03:06:09 +0900

    [add]GenarateManager.cs
    フィールドの生成方法を実装しました。
    The field generation method was implemented.

---
+++ Assets/GameScene/Scripts/GenarateManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hunkoro
+{
+    public enum FieldPattern
+    {
+
+    }
+
+    public class GenarateManager : MonoBehaviour
+    {
+        private new Rigidbody2D rigidbody;
+        [SerializeField]
+        private PlayerController player = null;
+        [SerializeField]
+        private UnkoGenarator unko = null;
+        [SerializeField]
+        private StoneGenarator stone = null;
+        [SerializeField]
+        private CactusGenarator cactus = null;
+        [SerializeField]
+        private GoalGenarator goal = null;
+        // Start is called before the first frame update
+        void Start()
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            rigidbody.velocity = new Vector2(0, player.GetSpeed());
+            //このGenaratorを一緒に動かすことで生成間隔を同期（？）する
+            if (transform.position.y < 0)
+            {
+                transform.position = new Vector3(0, transform.position.y + 10, 0);
+                GenarateField_1();
+            }
+        }
+
+        //生成するフィールドを決定する
+        void ChooseField()
+        {
+
+        }
+
+        void GenarateField_1()
+        {
+            unko.Genarate(new Vector3(-2.5f, 13f));
+            unko.Genarate(new Vector3(-2.5f, 6.5f));
+            unko.Genarate(new Vector3(-0.5f, 7.5f));
+            unko.Genarate(new Vector3(-0.5f, 10.5f));
+            unko.Genarate(new Vector3(0.5f, 13.5f));
+            unko.Genarate(new Vector3(1.5f, 9f));
+            unko.Genarate(new Vector3(2f, 15f));
+            stone.Genarate(new Vector3(-2f, 8.5f));
+            stone.Genarate(new Vector3(2f, 13f));
+            cactus.Genarate(new Vector3(-1.5f, 15f));
+            cactus.Genarate(new Vector3(-1f, 12.5f));
+            goal.Genarate(new Vector3(2.5f, 8f));
+        }
+    }
+}
--- Assets/GameScene/Scripts/StoneGenarator.cs
+++ Assets/GameScene/Scripts/StoneGenarator.cs
@@ -13,53 +13,18 @@ namespace Hunkoro
         private GameObject player = null;
         private PlayerController playerController;
 
-        //生成間隔
-        [SerializeField]
-        private float span = 0;
-        //間隔管理用変数
-        [SerializeField]
-        private float delta = 0;
         // Start is called before the first frame update
         void Start()
         {
             playerController = player.GetComponent<PlayerController>();
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-            //PLAY中のみ生成
-            if (playerController.GetGameMode() == GAMEMODE.PLAY)
-            {
-                //レベルデザイン？
-                switch (playerController.GetPlayerLevel())
-                {
-                    case PLAYER_LEVEL.Lv1:
-                        span = 2;
-                        break;
-                    case PLAYER_LEVEL.Lv2:
-                        break;
-                    case PLAYER_LEVEL.Lv3:
-                        break;
-                    case PLAYER_LEVEL.Lv4:
-                        break;
-                    case PLAYER_LEVEL.Lv5:
-                        break;
-                }
-                this.delta += Time.deltaTime;
-                if (this.delta > this.span)
-                {
-                    this.delta = 0;
-                    Genarate();
-                }
-            }
-        }
         //新しい石を生成する部分
-        private void Genarate()
+        public void Genarate(Vector3 pos)
         {
             //Prefabからインスタンスを生成
             StoneController genarate = Instantiate(stonePrefab);
-            genarate.transform.position = new Vector3(-2, 5, 0);
+            genarate.transform.position = pos;
             //新しいインスタンスにPlayerとPlayerControllerをセット
             genarate.player = player;
             genarate.playerController = playerController;
