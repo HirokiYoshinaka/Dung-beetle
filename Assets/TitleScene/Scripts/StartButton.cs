563760ef1881097300fd372f81d19bde18a80abe
YoshinakaHiroki <hiroki.yoshinaka777@gmail.com>
2019-09-09 04:50:43 +0900

    [update]StartButton
    押下時の効果音と一秒待機を追加しました。
    Added a sound effect when pressed and a one second wait.

--- Assets/TitleScene/Scripts/StartButton.cs
+++ Assets/TitleScene/Scripts/StartButton.cs
@@ -6,10 +6,20 @@ using UnityEngine.SceneManagement;
 namespace Hunkoro {
     public class StartButton : MonoBehaviour
     {
+        [SerializeField]
+        private AudioSource audioSource = null;
+        [SerializeField]
+        private AudioClip sound = null;
+
+        private void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
         //クリックしたとき
         private void OnMouseDown()
         {
-            SceneManager.LoadScene("GameScene");
+            StartCoroutine(LoadPlayScene());
         }
 
         private void OnMouseEnter()
@@ -20,6 +30,15 @@ namespace Hunkoro {
         private void OnMouseExit()
         {
             transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        private IEnumerator LoadPlayScene()
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            audioSource.PlayOneShot(sound);
+            yield return new WaitForSeconds(1);
+            SceneManager.LoadScene("GameScene");
+
         }
     }
 }
