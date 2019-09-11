ec94d2a223b44cb968c05e305009ed768564a837
YoshinakaHiroki <hiroki.yoshinaka777@gmail.com>
2019-09-12 07:08:19 +0900

    [update]
    スコア表示のアニメーションを追加しました。
    Added animation of score display.

--- Assets/ClearScene/Scripts/ClearScoreManager.cs
+++ Assets/ClearScene/Scripts/ClearScoreManager.cs
@@ -8,16 +8,36 @@ namespace Hunkoro
     public class ClearScoreManager : MonoBehaviour
     {
         private Text text = null;
+        private AudioSource audioSource = null;
+        [SerializeField]
+        private AudioClip audioClip = null;
         // Start is called before the first frame update
         void Start()
         {
             text = GetComponent<Text>();
+            audioSource = GetComponent<AudioSource>();
+            StartCoroutine(dispScore());
         }
 
         // Update is called once per frame
         void Update()
         {
-            text.text = PlayerController.GetUnkoScore().ToString();
+
+            //text.text = PlayerController.GetUnkoScore().ToString();
+        }
+
+        private IEnumerator dispScore()
+        {
+            int s = 0;
+            while (s < PlayerController.GetUnkoScore())
+            {
+                yield return new WaitForSeconds(0.1f);
+                s++;
+                text.text = s.ToString();
+                audioSource.PlayOneShot(audioClip);
+            }
+            GameObject next = transform.Find("Time").gameObject;
+            next.SetActive(true);
         }
     }
 }
--- Assets/ClearScene/Scripts/ClearTimeManager.cs
+++ Assets/ClearScene/Scripts/ClearTimeManager.cs
@@ -9,18 +9,41 @@ namespace Hunkoro
     public class ClearTimeManager : MonoBehaviour
     {
         private Text text = null;
+        private AudioSource audioSource = null;
+        [SerializeField]
+        private AudioClip count = null;
+        [SerializeField]
+        private AudioClip end = null;
         // Start is called before the first frame update
         void Start()
         {
             text = GetComponent<Text>();
+            audioSource = GetComponent<AudioSource>();
+            StartCoroutine(dispScore());
         }
 
         // Update is called once per frame
         void Update()
         {
+
+        }
+
+        private IEnumerator dispScore()
+        {
             float time = PlayerController.GetTimeScore();
             time = Mathf.Round(time * 10) / 10;
+            float t = 0;
+
+            while (t < time)
+            {
+                yield return null;
+                text.text = String.Format("{0:0.0}", t);
+                t += time / 60;
+                audioSource.PlayOneShot(count);
+            }
+            yield return null;
             text.text = String.Format("{0:0.0}", time);
+            audioSource.PlayOneShot(end);
         }
     }
 }
