90e8241efc19dd7397a5fd4d92196ad3e0088717
YoshinakaHiroki <hiroki.yoshinaka777@gmail.com>
2019-09-16 01:35:48 +0900

    [add]
    "GoldUnko"のController,prefab,Genaratorの実装

---
+++ Assets/GameScene/Scripts/GoldUnkoGenarator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hunkoro
+{
+    namespace GameScene
+    {
+        public class GoldUnkoGenarator : MonoBehaviour
+        {
+            [SerializeField]
+            private GoldUnkoController goldUnkoPrefab = null;
+            [SerializeField]
+            private GameObject player = null;
+            private PlayerController playerController;
+
+            // Start is called before the first frame update
+            void Start()
+            {
+                playerController = player.GetComponent<PlayerController>();
+            }
+
+            //新しいうんこを生成する部分
+            public void Genarate(float x, float y)
+            {
+                Genarate(new Vector3(x, y, 0));
+            }
+            public void Genarate(Vector3 pos)
+            {
+                //PrefabからGameObjectを生成
+                GoldUnkoController genarate = Instantiate(goldUnkoPrefab);
+                genarate.transform.position = pos;
+                //新しいうんこにPlayerとPlay
+                genarate.player = player;
+                genarate.playerController = playerController;
+            }
+        }
+    }
+}
