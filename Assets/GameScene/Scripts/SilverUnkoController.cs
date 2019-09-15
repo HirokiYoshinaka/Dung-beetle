523ddb7129fa6501d5ccde96b759f1e06863a5d2
YoshinakaHiroki <hiroki.yoshinaka777@gmail.com>
2019-09-16 01:23:02 +0900

    [add]
    SilverUnkoController.csの実装

---
+++ Assets/GameScene/Scripts/SilverUnkoController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hunkoro
+{
+    namespace GameScene
+    {
+        public class SilverUnkoController : MonoBehaviour
+        {
+            //Genaratorからセットするのでpublicに
+            public GameObject player = null;
+            public PlayerController playerController = null;
+            private new Rigidbody2D rigidbody;
+
+            //SE
+            public AudioClip Sound = null;
+            AudioSource audioSource = null;
+            private SpriteRenderer spriteRenderer = null;
+
+            // Start is called before the first frame update
+            void Start()
+            {
+                //Genaratorからセットするので不要（この設計まずいか？
+                //player = GameObject.Find("Player");
+                //playerController = player.GetComponent<PlayerController>();
+                this.rigidbody = GetComponent<Rigidbody2D>();
+                audioSource = gameObject.GetComponent<AudioSource>();
+                spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+                rigidbody.velocity = new Vector2(0, playerController.GetSpeed());
+                //画面外
+                if (transform.position.y < -6)
+                {
+                    //オブジェクトのDestroy
+                    Destroy(gameObject);
+                }
+            }
+
+
+            //当たり判定の処理部分
+            private void OnTriggerEnter2D(Collider2D collision)
+            {
+                if (collision.tag == "Player")
+                {
+                    //SEを鳴らす
+                    audioSource.PlayOneShot(Sound);
+
+                    //ここでDestroyすると音まで消えてしまうので
+                    //Destroy(gameObject);
+                    //spriteを非表示にして対応
+                    //画面外に出た際にお片付けするのでこれでおｋ
+                    spriteRenderer.enabled = false;
+                    GetComponent<BoxCollider2D>().enabled = false;
+                }
+            }
+        }
+    }
+}
